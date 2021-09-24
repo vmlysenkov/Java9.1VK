1d13804804357bf1ebae12e76f8c3b36e2b5a164
Vasilii Lysenkov <lysenkov2@ya.ru>
2021-09-24 12:59:23 +0300

    Added manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,16 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+
+
+}
